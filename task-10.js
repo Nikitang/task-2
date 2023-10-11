6822348740b926fd97cc7c057856843693d885cc
Nikita <gerasimenya.05@mail.ru>
2023-10-11 17:26:10 +0300

    task-10

---
+++ task-10.js
@@ -0,0 +1,5 @@
+const allmoney = 2000;
+const friend = 5;
+const check = allmoney / friend;
+const tea = 500;
+console.log(`С каждого по ${check} рублей, при общем счёте ${allmoney} рублей и чаевыми ${tea} рублей.`)
